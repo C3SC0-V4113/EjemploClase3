a08d05c1095cf095f697abef8955b57dfb44af9d
Francisco Valle <vallecesco@gmail.com>
2021-08-16 12:27:22 -0600

    Pagina FUncional

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Image } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+
+const estilos=StyleSheet.create({
+  grandeAzul:{
+    color:'blue',
+    fontWeight:'bold',
+    fontSize:'60',
+  },
+  rojo:{
+    color:'red',
+  },
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={
+          {uri:'https://reactjs.org/logo-og.png'}
+        }
+        style={{width:400,height:400}}
+      />
+      <Text style={estilos.rojo}>Solo rojo</Text>
+      <Text style={estilos.grandeAzul}>Solo Azul grande</Text>
+      <Text style={[estilos.grandeAzul,estilos.rojo]}>Azul grande, entonces rojo</Text>
+      <Text style={[estilos.rojo,estilos.grandeAzul]}>rojo, entoces Azul grande</Text>
     </View>
   );
 }
